a07debe22d0ddd9a660c9a46ab179551125dd709
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-09-21 20:36:09 -0400

    Creadas multiples rutas

---
+++ app/tienda/categorias/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Categorias() {
+    return(
+        <>
+            <h2>Categorias</h2>
+
+            <ul>
+                <li>
+                    producto 1
+                </li>
+            </ul>
+        
+        </> 
+    );
+}
