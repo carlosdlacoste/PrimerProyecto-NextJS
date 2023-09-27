d5b8538a44d7972223bab119403a186039578c60
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-09-26 22:41:18 -0400

    Aplicando use client a los componentes client de la app

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,35 @@
+"use client";
+import Users from "../components/users";
+import { useEffect, useRef } from "react";
+
 const Page = () => {
-    return <h1>Hello World</h1>
-}
+    
+    const isMounted = useRef(false);
+
+    useEffect(() => {
+        // es importante revisar esto ya que es una solucion temporal a que el useEffect se ejecute mas de una vez pero no resuelve el problema de raiz
+        if (isMounted.current) {
+            alert("loaded!")
+            console.log('useEffect se ejecutó');
+        } else {
+        isMounted.current = true;
+        }
+
+        // alert("Loaded!")
+    }, []);
+
+    return(
+        <>
+        
+            <h1>Hello World</h1>
+            <button onClick={() => {
+                alert("Hello!")
+            }}>Click</button>
+            <section>
+                <Users/>
+            </section>
+        </>
+    );
+} 
 
 export default Page;
