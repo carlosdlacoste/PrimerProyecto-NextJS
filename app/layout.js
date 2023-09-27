f214432440ec1f83220ded4faf44c4c8228352be
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-09-26 20:16:16 -0400

    Cambiando el tipo de letra importando la libreria next/font/google

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,22 @@
 import Navbar from "../components/navbar.jsx";
+import {Roboto} from 'next/font/google';
 
 export const metadata = {
   title: 'Mi Tienda con Next.js - HomePage',
   description: 'Esta es la pagina principal de mi tienda',
   keywords: "tienda, online, ecommerce"
-}
+};
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  styles: ["italic", "normal"],
+  subsets: ["latin"]
+})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Navbar/>
         {children}
       </body>
