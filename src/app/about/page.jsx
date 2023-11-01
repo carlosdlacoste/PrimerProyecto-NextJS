edd378799f9ec72a5ffaba028374a5479752502a
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-01 01:30:50 -0400

    Metadata renderizada correctamente en la pagina About

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,18 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-    
+const metadata = {
+    title: 'Acerca de nosotros',
+};
 
-const AboutPage = ({metadata}) => {
+const AboutPage = () => {
     const router = useRouter();
 
     return (
         <>
+            <head>
+                <title>{metadata.title}</title>
+            </head>
             <h1>About</h1>
             <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
                 The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
