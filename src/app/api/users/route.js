e2aba4970d5555e7175922040cc60614dcacced9
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-05 15:13:05 -0400

    Aplicando variables de entorno para proteger datos sensibles

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 
-
+console.log(process.env.TOKEN)
 //extract params
 // database queries
 // communicate with other services
 
 export async function GET() {
-    const resp = await fetch("https://jsonplaceholder.typicode.com/users")
+    const resp = await fetch("https://jsonplaceholder.typicode.com/users?apikey=" + process.env.TOKEN)
+    console.log("https://jsonplaceholder.typicode.com/users?apikey=" + process.env.TOKEN)
     const data = await resp.json()
     return NextResponse.json(data)
 }
