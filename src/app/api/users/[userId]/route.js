0d5cce117750c1931d3c5ec964f614d224dca72e
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-05 14:33:48 -0400

    Aplicando searchParams y request para encontrar coincidencias de nombre y apellido directamente en la url

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request, {params}) {
+    const {searchParams} = new URL(request.url)
+    console.log(searchParams)
+    console.log(searchParams.get('nombre'))
+    console.log(searchParams.get('apellido'))
     const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`)
     const data = await resp.json()
     return NextResponse.json(data)
